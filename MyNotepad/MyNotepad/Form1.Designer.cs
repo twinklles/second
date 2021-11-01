655df152396a42faf482112eababbdcdd6e76f58
Arina Sitdikova <arina.sitdikova228@gmail.com>
2021-11-01 23:18:43 +0500

    Revert "зеленая форма и нет кнопки закрыть"
    This reverts commit 02be4763c630d58cdd922d69f2d0fc6e395bfd31.

--- MyNotepad/MyNotepad/Form1.Designer.cs
+++ MyNotepad/MyNotepad/Form1.Designer.cs
@@ -28,14 +28,26 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(273, 97);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Закрыть ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.BackColor = System.Drawing.Color.SandyBrown;
             this.ClientSize = new System.Drawing.Size(627, 233);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот ";
             this.ResumeLayout(false);
@@ -43,6 +55,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
